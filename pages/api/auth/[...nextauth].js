fec76a169438e7b3c458487e61a79b3e70bf0a7d
Anthony Hadwan <anthony.hadwan@hec.edu>
2023-02-21 00:46:28 +0100

    remove token persistance to the client side

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -24,11 +24,6 @@ export default NextAuth({
       }
       return token;
     },
-    async session({ session, token, user }) {
-      // Send properties to the client, like an access_token from a provider.
-      session.accessToken = token.accessToken;
-      return session;
-    },
   },
 });
 
