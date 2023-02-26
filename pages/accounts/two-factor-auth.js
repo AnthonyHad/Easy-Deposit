c83ac7c28c7e29838223bdd713175da269c66970
Anthony Hadwan <anthony.hadwan@hec.edu>
2023-02-27 00:18:41 +0100

    added twoFA to transaction object and added code to send updated request to send route

--- pages/accounts/two-factor-auth.js
+++ pages/accounts/two-factor-auth.js
@@ -1,12 +1,35 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function TwoFactorAuth() {
+  const [transactionData, setTransactionData] = useState(null);
   const [twoFactorCode, setTwoFactoreCode] = useState('');
 
-  function handleTwoFactorSubmit() {
+  useEffect(() => {
+    const storedData = localStorage.getItem('transactionData');
+    if (storedData) {
+      setTransactionData(JSON.parse(storedData));
+    }
+  }, []);
+
+  function handleTwoFactorSubmit(event) {
     event.preventDefault();
 
-    setTwoFactoreCode('');
+    // Add the 2FA code to the transaction data
+    transactionData.twoFactorCode = twoFactorCode;
+    delete transactionData.requiresTwoFactor;
+    console.log(transactionData);
+
+    fetch('/api/send', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/html',
+      },
+      body: JSON.stringify(transactionData),
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+
+    // setTwoFactoreCode('');
   }
 
   function handleTwoFactorChange(event) {
