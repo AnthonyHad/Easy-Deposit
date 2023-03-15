1011305b06d07a69a962b505a884cd8c9160dff7
Anthony Hadwan <anthony.hadwan@hec.edu>
2023-03-15 22:18:50 +0100

    added styling to the cancel transction button

--- pages/accounts/two-factor-auth.js
+++ pages/accounts/two-factor-auth.js
@@ -58,13 +58,20 @@ function TwoFactorAuth() {
           onChange={handleTwoFactorChange}
           class="w-48 outline-none rounded-lg px-3 py-2 mb-4 text-center"
         />
-        <button
-          type="submit"
-          class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2"
-        >
-          Submit
-        </button>
-        <button onClick={cancelTansaction}>Cancel Transaction</button>
+        <div class="flex justify-between ">
+          <button
+            type="submit"
+            class="flex-1 px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2 mr-2"
+          >
+            Submit
+          </button>
+          <button
+            className='flex-1 px-4 py-1 text-sm text-red-500 font-semibold rounded-full border border-red-500 hover:text-white hover:bg-red-500 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 ml-2"'
+            onClick={cancelTansaction}
+          >
+            Cancel
+          </button>
+        </div>
       </form>
     </Fragment>
   );
